785217281bf2ba087d68ef504499491cdcee8243
Justin Dugger <jldugger@osuosl.org>
2014-07-31 09:16:37 -0700

    add local version bumps

--- moosh.sh
+++ moosh.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 NAME="moosh"
-VERSION="0.15.2-2"
+REMOTEVERSION="0.15.2-2"
+VERSION=$REMOTEVERSION'osl1'
 URL="http://moosh-online.com/"
 VENDOR="OSU Open Source Lab"
 LICENSE="GPLv3"
@@ -12,10 +13,10 @@ TOPDIR=`dirname $( readlink -f $0 )`
 FPM="fpm"
 
 # download deb we intend to convert
-wget http://ppa.launchpad.net/zabuch/ppa/ubuntu/pool/main/m/${NAME}/${NAME}_${VERSION}_all.deb
+wget http://ppa.launchpad.net/zabuch/ppa/ubuntu/pool/main/m/${NAME}/${NAME}_${REMOTEVERSION}_all.deb
 
 # create rpm package
 ${FPM} -f -s deb -t rpm -n ${NAME} -v ${VERSION} --url ${URL} \
     -m repos@osuosl.org --vendor "${VENDOR}" --license ${LICENSE} \
     --description "${DESCRIPTION}" --prefix /usr/bin --depends "php-cli" \
-    --depends "php53u-process" --no-auto-depends ${NAME}_${VERSION}_all.deb
+    --depends "php53u-process" --no-auto-depends ${NAME}_${REMOTEVERSION}_all.deb
